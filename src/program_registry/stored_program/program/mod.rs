bc28d4e4ec8e1fb603f9eaa68fc87403a3918e3f
isaac <isaacloff@gmail.com>
2026-05-03 18:26:19 +0100

    program registry generics

--- src/program_registry/stored_program/program/mod.rs
+++ src/program_registry/stored_program/program/mod.rs
@@ -1,1 +1,19 @@
-pub struct Program;
+use aion_state::prelude::Registry;
+
+use crate::prelude::{AccessStorage, BlacklistStorage, ControlStorage, CredentialStorage, RegistryStorage, ReservationStorage, WhitelistStorage};
+
+pub mod resource_id;
+pub mod stored_resource;
+pub mod resource_access;
+
+pub struct Program {
+    registry: Registry<
+        RegistryStorage<ResourceId, StoredResource>,
+        ReservationStorage<ResourceId, ResourceAccess>,
+        AccessStorage<ResourceId, ResourceAccess>,
+        CredentialStorage,
+        WhitelistStorage<ResourceId, ResourceAccess>,
+        BlacklistStorage<ResourceId, ResourceAccess>,
+        ControlStorage<ResourceId>
+    >
+}
