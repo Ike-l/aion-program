5212b06e91a1b1239e640e97656b1278c25b449c
isaac <isaacloff@gmail.com>
2026-05-03 16:54:06 +0100

    access builder

--- src/injection/access_builder/mod.rs
+++ src/injection/access_builder/mod.rs
@@ -1,4 +1,4 @@
-use crate::prelude::{Access, ProgramId, ResourceId, ResourcePassword, UserId, UserPassword};
+use crate::prelude::{Access, FinalisedAccess, ProgramId, ResourceId, ResourcePassword, UserId, UserPassword};
 
 pub struct AccessBuilder {
     program_id: ProgramId,
@@ -9,4 +9,23 @@ pub struct AccessBuilder {
     resource_id: ResourceId,
     resource_password: Option<ResourcePassword>,
     access: Access,
+
+    use_global_program_id: bool,
+}
+
+impl AccessBuilder {
+    pub fn build(self) -> FinalisedAccess {
+        let program_id = match self.use_global_program_id {
+            true => self.global_program_id,
+            false => self.program_id,
+        };
+
+        FinalisedAccess::new(
+            program_id, 
+            self.user_details, 
+            self.resource_id, 
+            self.resource_password, 
+            self.access
+        )
+    }
 }
