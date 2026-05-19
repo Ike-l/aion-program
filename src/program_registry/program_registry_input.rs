46d6810706e70b622a744b4985e04eefe734a432
isaac <isaacloff@gmail.com>
2026-05-19 15:38:47 +0100

    Fix: made resource closure Send

--- src/program_registry/program_registry_input.rs
+++ src/program_registry/program_registry_input.rs
@@ -28,7 +28,7 @@ pub struct ProgramRegistryResolveWithInsert<'a> {
     pub program_id: Option<ProgramId>,
     pub program_password: Option<&'a ValuePassword>,
 
-    pub resource: Option<Box<dyn FnOnce() -> Resource>>,
+    pub resource: Option<Box<dyn FnOnce() -> Resource + Send>>,
     pub resource_id: Option<ResourceId>,
     pub resource_password: Option<&'a ValuePassword>
 }
