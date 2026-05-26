107fb18ad5617efa8663e908c92e9cfc1871ba43
isaac <isaacloff@gmail.com>
2026-05-26 18:59:06 +0100

    Feat: resolve_resource_error: messages & Display

--- src/injection/resolve_resource_error/mod.rs
+++ src/injection/resolve_resource_error/mod.rs
@@ -1,7 +1,22 @@
+use std::fmt::Display;
+
 #[derive(Debug, PartialEq)]
 pub enum ResolveResourceError {
-    Casting,
-    Resolving,
-    NotEnoughResults,
-    TooManyResults,
+    Casting(String),
+    Resolving(String),
+    NotEnoughResults(String),
+    TooManyResults(String),
+}
+
+impl Display for ResolveResourceError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let s = match self {
+            ResolveResourceError::Casting(msg) => format!("Casting: {}", msg),
+            ResolveResourceError::Resolving(msg) => format!("Resolving: {}", msg),
+            ResolveResourceError::NotEnoughResults(msg) => format!("NotEnoughResults: {}", msg),
+            ResolveResourceError::TooManyResults(msg) => format!("TooManyResults: {}", msg),
+        };
+
+        write!(f, "{}", s)
+    }
 }
