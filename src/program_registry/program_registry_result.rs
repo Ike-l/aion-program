845519a6c84f4d1c7e3cd5061e25e473fe62461c
isaac <isaacloff@gmail.com>
2026-05-26 19:00:19 +0100

    Feat: Display on ProgramRegistryResolveError

--- src/program_registry/program_registry_result.rs
+++ src/program_registry/program_registry_result.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 use crate::prelude::{AccessSubmissionError, ResolveResourceError};
 
 pub enum ProgramRegistryReplaceResourceError {
@@ -28,6 +30,29 @@ pub enum ProgramRegistryResolveWithInsertError {
 pub enum ProgramRegistryResolveError {
     AccessSubmissionError(AccessSubmissionError),
     ResolveResourceError(ResolveResourceError),
+}
+
+impl Display for ProgramRegistryResolveError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let s = match self {
+            ProgramRegistryResolveError::AccessSubmissionError(access_submission_error) => {
+                match access_submission_error {
+                    AccessSubmissionError::NotEnoughPrompts(msg) => format!("NotEnoughPrompts: {}", msg),
+                    AccessSubmissionError::TooManyPrompts(msg) => format!("TooManyPrompts: {}", msg),
+                }
+            },
+            ProgramRegistryResolveError::ResolveResourceError(resolve_resource_error) => {
+                match resolve_resource_error {
+                    ResolveResourceError::Casting(msg) => format!("Casting: {}", msg),
+                    ResolveResourceError::Resolving(msg) => format!("Resolving: {}", msg),
+                    ResolveResourceError::NotEnoughResults(msg) => format!("NotEnoughResults: {}", msg),
+                    ResolveResourceError::TooManyResults(msg) => format!("TooManyResults: {}", msg),
+                }
+            },
+        };
+
+        write!(f, "{}", s)
+    }
 }
 
 pub enum ProgramRegistryResolveAsyncError {
