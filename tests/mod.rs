793279f8e8b7d100d91cba5d029a0a36f485c794
isaac <isaacloff@gmail.com>
2026-05-26 21:05:21 +0100

    Test: Resolve with Insert passes

--- tests/mod.rs
+++ tests/mod.rs
@@ -1,19 +1,55 @@
-// use aion_program::program_registry::ProgramRegistry;
-// use aion_program::program_registry::injected::injected_primitives::shared::Shared;
-// use aion_program::program_registry::program::program_results::ProgramResolveResult;
-// use aion_program::program_registry::program_registry_results::ProgramRegistryResolveResult;
+use std::{any::TypeId, sync::Arc};
 
-// fn foo() {
-//     let program_registry = ProgramRegistry::default();
+use aion_program::prelude::{ProgramRegistry, ProgramRegistryResolveWithInsert, Resource, ResourceId, Shared};
 
-//     let program_id = None;
-//     let resource_id = None;
-//     let program_reserver_id = None;
-//     let resource_reserver_id = None;
-//     let program_key_id = None;
-//     let resource_key_id = None;
+use tracing::span;
+use tracing_subscriber::{EnvFilter, fmt};
+use std::sync::Once;
 
-//     let shared = program_registry.resolve::<Shared<bool>>(program_id, resource_id, program_reserver_id, resource_reserver_id, program_key_id, resource_key_id);
-//     let ProgramRegistryResolveResult::Found(ProgramResolveResult::Found(Ok(shared))) = shared else { panic!() };
-//     let a = shared.as_ref();
-// }
+static INIT: Once = Once::new();
+
+fn init_tracing() {
+    INIT.call_once(|| {
+        // let filter = EnvFilter::from_default_env()
+        //     .add_directive(tracing::Level::DEBUG.into())
+        //     .add_directive("aion_state=warn".parse().unwrap());
+
+        fmt()
+            .with_ansi(false)
+            .compact()
+            .pretty()
+            .with_max_level(tracing::Level::TRACE)
+            // .with_env_filter(filter)
+            // .with_span_events(fmt::format::FmtSpan::ENTER | fmt::format::FmtSpan::EXIT)
+            .with_target(false)
+            .with_test_writer()           
+            .init();
+    });
+    // .with_env_filter(EnvFilter::new("info,aion_state=info"))
+}
+
+#[test]
+fn foo() {
+    init_tracing();
+
+    let program_registry = Arc::new(ProgramRegistry::default());
+
+    let result = {
+        let span = span!(tracing::Level::INFO, "Getting bool");
+        let _enter = span.enter();
+    
+        program_registry.resolve_with_insert::<Shared<bool>>(None, vec![], ProgramRegistryResolveWithInsert {
+            resource: Some(Box::new(|| Resource::new(true))),
+            resource_id: Some(ResourceId::TypeId(TypeId::of::<bool>())),
+            ..Default::default()
+        })
+    };
+
+    match result {
+        Ok(item) => {
+            assert!(item.as_ref());
+            drop(item);
+        },
+        Err(err) => panic!("{err}"),
+    }
+}
