6627fc9f4b773658c41528ce9bfa0e0799f92725
isaac <isaacloff@gmail.com>
2026-05-26 19:20:29 +0100

    Feat: Display on access_submission_error

--- src/injection/access_submission_error/mod.rs
+++ src/injection/access_submission_error/mod.rs
@@ -1,4 +1,17 @@
+use std::fmt::Display;
+
 pub enum AccessSubmissionError {
     NotEnoughPrompts(String),
     TooManyPrompts(String)
+}
+
+impl Display for AccessSubmissionError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let s = match self {
+            Self::NotEnoughPrompts(msg) => format!("NotEnoughPrompts: {}", msg),
+            Self::TooManyPrompts(msg) => format!("TooManyPrompts: {}", msg),
+        };
+
+        write!(f, "{}", s)
+    }
 }
